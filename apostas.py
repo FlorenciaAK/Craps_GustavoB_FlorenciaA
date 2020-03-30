29666669894d2d1be278a90e3927fb1d88a086a9
Florencia Averame <Flor@MacBook-Air-de-Florencia-97.local>
2020-03-30 14:17:16 -0300

    começando as apostas

---
+++ apostas.py
@@ -0,0 +1,7 @@
+print ("Você está jogando")
+
+while continuar and fichas_suficientes:
+    if continuar=="continuar":
+        continuar=True
+    else:
+        continuar=False
---
+++ parametros.py
@@ -0,0 +1,7 @@
+fichas = 50 #por exemplo já que a gente decide
+fichas_disponíveis = print ("Você possui {0} fichas".format(fichas))
+continuar = ("Você deseja continuar ou abandonar o jogo? (escolher entre continuar ou abandonar: ")
+continuar = True
+fichas_suficientes=True #fichas disponíveis > 0 
+
+import random
