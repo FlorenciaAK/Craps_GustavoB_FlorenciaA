417e8370e2af0c6d2541f4ded1df802b26beec97
Florencia Averame <Flor@MacBook-Air-de-Florencia-97.local>
2020-03-30 14:19:33 -0300

    apostas- condições

--- apostas.py
+++ apostas.py
@@ -3,5 +3,9 @@ print ("Você está jogando")
 while continuar and fichas_suficientes:
     if continuar=="continuar":
         continuar=True
+        if fichas>0:
+            fichas_suficientes=True
+        else:
+            fichas_suficientes=False
     else:
         continuar=False
