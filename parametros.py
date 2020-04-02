357f596e8ad62918cc9bc7f4818c8263cbe8c2a7
Florencia Averame <Flor@MacBook-Air-de-Florencia-97.local>
2020-04-02 19:57:27 -0300

    código finalizado

--- parametros.py
+++ parametros.py
@@ -1,5 +1,7 @@
-fichas = 50 #por exemplo já que a gente decide
-fichas_suficientes = fichas>0 
-
-
 import random
+
+fichas = 50 #por exemplo já que a gente decide
+
+jogar_dados = False
+
+print ("Você está jogando")
